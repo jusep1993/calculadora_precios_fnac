32f9a4ef4854df954b9f5c53d879779907f66d47
jusep1993 <joseantoniolopezbenitez93@gmail.com>
2022-06-21 17:54:51 +0200

    Fix the membership bug calculator

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ from tkinter import ttk
 
 
 def app():
-    def calculate():
-        if b_is_member:
+    def cal_membership():
+        if b_is_member.get() == "0":
+            d_total_price.set(d_entry_product_price.get())
+
+        if b_is_member.get() == "1":
             temp = float(d_entry_product_price.get()) * 0.05
             temp = round(temp, 2)
 
@@ -13,45 +16,42 @@ def app():
             result = round(result, 2)
             d_total_price.set(str(result))
 
-        if want_financing:
-            if list_months.get() == "3 Meses":
-                temp = float(d_total_price.get()) / 3
-                result = round(temp, 2)
-                d_total_monthly_fees.set(str(result))
-
-            elif list_months.get() == "10 Meses":
-                temp = float(d_total_price.get()) / 10
-                result = round(temp, 2)
-                d_total_monthly_fees.set(str(result))
-
-            elif list_months.get() == "20 Meses":
-                temp = float(d_total_price.get()) / 20
-                result = round(temp, 2)
-                d_total_monthly_fees.set(str(result))
-
-        else:
-            d_total_monthly_fees.set(" ")
-
-
-
-    # def is_member():
-    #     if b_is_member:
-    #         financin_yes_rbtn.config(bg="blue")
-    #         financin_no_rbtn.config(state="normal")
+    # def cal_financing():
+    #     if want_financing == "1":
+    #         if list_months.get() == "3 Meses":
+    #             temp = float(d_total_price.get()) / 3
+    #             result = round(temp, 2)
+    #             d_total_monthly_fees.set(str(result))
     #
-    #     if  b_is_member == False:
-    #         financin_yes_rbtn.config(bg="#808080")
-    #         financin_no_rbtn.config(state="disabled")
+    #         elif list_months.get() == "10 Meses":
+    #             temp = float(d_total_price.get()) / 10
+    #             result = round(temp, 2)
+    #             d_total_monthly_fees.set(str(result))
+    #
+    #         elif list_months.get() == "20 Meses":
+    #             temp = float(d_total_price.get()) / 20
+    #             result = round(temp, 2)
+    #             d_total_monthly_fees.set(str(result))
+    #
+    #     else:
+    #         d_total_monthly_fees.set(" ")
+
+    def calculate():
+        cal_membership()
+        # cal_financing()
+
 
     window = tkinter.Tk()
 
+    # ***********************************************************
     # Variables
     d_entry_product_price = tkinter.StringVar()
     d_total_price = tkinter.StringVar()
     d_total_monthly_fees = tkinter.StringVar()
-    b_is_member = tkinter.BooleanVar()
-    b_want_financing = tkinter.BooleanVar()
+    b_is_member = tkinter.StringVar()
+    b_want_financing = tkinter.StringVar()
     list_financing_months = ["3 Meses", "10 Meses", "20 Meses"]
+    # ***********************************************************
 
     window.geometry("500x300")
     window.config(background="#808080")
@@ -79,28 +79,28 @@ def app():
     are_you_member = tkinter.Label(frame, text="Tienes carnet de Socio del Fnac?", font=("Arial", 15))
     are_you_member.grid(row=1, column=0, columnspan=2, sticky="w")
 
-    member_yes_rbtn = tkinter.Radiobutton(frame, text="Si", variable=b_is_member, value=True)
-    member_no_rbtn = tkinter.Radiobutton(frame, text="No", variable=b_is_member, value=False)
+    member_yes_rbtn = tkinter.Radiobutton(frame, text="Si", variable=b_is_member, value="1")
+    member_no_rbtn = tkinter.Radiobutton(frame, text="No", variable=b_is_member, value="0")
     member_yes_rbtn.grid(row=1, column=1, sticky="")
     member_no_rbtn.grid(row=1, column=2)
 
     # Linea prgunta financiacion
-    want_financing = tkinter.Label(frame, text="¿Quieres financiar el pago?", font=("Arial", 15))
-    want_financing.grid(row=2, column=0)
-    financin_yes_rbtn = tkinter.Radiobutton(frame, text="Si", variable=b_want_financing, value=True)
-    financin_no_rbtn = tkinter.Radiobutton(frame, text="No", variable=b_want_financing, value=False)
-    financin_yes_rbtn.grid(row=2, column=1)
-    financin_no_rbtn.grid(row=2, column=2)
+    # want_financing = tkinter.Label(frame, text="¿Quieres financiar el pago?", font=("Arial", 15))
+    # want_financing.grid(row=2, column=0)
+    # financin_yes_rbtn = tkinter.Radiobutton(frame, text="Si", variable=b_want_financing, value="1")
+    # financin_no_rbtn = tkinter.Radiobutton(frame, text="No", variable=b_want_financing, value="0")
+    # financin_yes_rbtn.grid(row=2, column=1)
+    # financin_no_rbtn.grid(row=2, column=2)
 
     # Añadir un if no socio no puedes financiar
 
     # Linea de a cuantos meses la financiacion
-    many_months = tkinter.Label(frame, text="¿A cuantos meses quieres financiar?", font=("Arial", 15))
-    many_months.grid(row=3, column=0, columnspan=2, sticky="w")
-
-    list_months = ttk.Combobox(frame, values=list_financing_months, width=8)
-    list_months.set("Meses")
-    list_months.grid(row=3, column=1, sticky="e")
+    # many_months = tkinter.Label(frame, text="¿A cuantos meses quieres financiar?", font=("Arial", 15))
+    # many_months.grid(row=3, column=0, columnspan=2, sticky="w")
+    #
+    # list_months = ttk.Combobox(frame, values=list_financing_months, width=8)
+    # list_months.set("Meses")
+    # list_months.grid(row=3, column=1, sticky="e")
 
     # Añadir funcion para recuperar la seleccion
 
@@ -125,14 +125,14 @@ def app():
 
     # Linea total mensualidades
 
-    monthly_fees = tkinter.Label(frame, text="Mensualidades:", font=("Arial", 15))
-    monthly_fees.grid(row=7, column=0, sticky="w")
-
-    total_monthly_fees = tkinter.Label(frame, textvariable=d_total_monthly_fees, font=("Arial", 15), relief="sunken")
-    total_monthly_fees.grid(row=7, column=1, sticky="w")
-
-    euros_symbol_3 = tkinter.Label(frame, text="€/mes", font=("Arial", 15))
-    euros_symbol_3.grid(row=7, column=1, sticky="e")
+    # monthly_fees = tkinter.Label(frame, text="Mensualidades:", font=("Arial", 15))
+    # monthly_fees.grid(row=7, column=0, sticky="w")
+    #
+    # total_monthly_fees = tkinter.Label(frame, textvariable=d_total_monthly_fees, font=("Arial", 15), relief="sunken")
+    # total_monthly_fees.grid(row=7, column=1, sticky="w")
+    #
+    # euros_symbol_3 = tkinter.Label(frame, text="€/mes", font=("Arial", 15))
+    # euros_symbol_3.grid(row=7, column=1, sticky="e")
 
     # window.update()
     window.mainloop()
